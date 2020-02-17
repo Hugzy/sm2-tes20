f325e299b9ba5f11fb3a9cee412e4be3b65edcbf
frederik madsen <magida@DESKTOP-V4VMNLF.DigiDom.dk>
2020-02-17 12:58:23 +0100

    lecture 2

--- lec02/class_excersices.ml
+++ lec02/class_excersices.ml
@@ -14,7 +14,7 @@ let rec member needle haystack = match haystack with
     | [] -> false
     | hay::haystack -> match hay with 
                     | needle -> true
-                    | _ -> member haystack needle;;
+                    | _ -> member needle haystack;;
 
 let fst n = match n with
     | (x,_) -> x
@@ -26,13 +26,24 @@ let mytest = Test.make float (fun f -> floor f <= f);;
 
 let sum_test = Test.make ~name: "sum" (pair (list int) (list int)) (fun(xs,xy) -> sum(xs@xy) = sum(xs) + sum(xy));;
 
-let rec merge l1 l2 = match l1, l2 with
-    | ([], []) -> []
-    | (l, []) -> l
+
+(*
+Properties that should hold
+- Output should always be sorted
+ *)
+let rec merge lhs rhs = match lhs, rhs with
+    | (l, [])
     | ([], l) -> l
     | el1::elems1, el2::elems2 -> match el1 > el2 with
-        | true -> el2::merge l1 elems2
-        | false -> el1::merge elems1 l2;;
+        | true -> el2::merge lhs elems2
+        | false -> el1::merge elems1 rhs;;
 
-    
-QCheck_runner.run_tests [mytest];;
+let merge_test = Test.make ~name: "merge" ~count:1000 ~max_gen:1000 (pair (list int) (list int)) (fun(lhs, rhs) -> 
+    let sorted_lhs = List.sort compare lhs in
+    let sorted_rhs = List.sort compare rhs in
+    merge sorted_lhs sorted_rhs = List.sort compare (lhs@rhs));;
+
+
+let print_test = Test.make ~name:"print" string (fun(str) -> print_string str; true = true);;
+
+QCheck_runner.run_tests [mytest; merge_test; print_test];;
